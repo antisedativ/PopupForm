cda9c70d78d3e5adbd01c70e598a060a1e854573
Satira <90534931+ArtemMazyev210@users.noreply.github.com>
2021-10-25 21:54:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.popup').magnificPopup();
+
+
+    $('#form').submit(function () {
+        $.ajax({
+            type: 'POST',
+            url: 'server.php',
+            data: $(this).serialize()
+        }).done(function () {
+            alert('Thank you!');
+            setTimeout(function () {
+                $.magnificPopup.close();
+            }, 1000);
+        });
+    });
+    return false;
+});
